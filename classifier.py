62760c5eaff4e59afa6c6201fac5a848d78106a9
Chen YiJia <65040076+chence08@users.noreply.github.com>
2022-07-09 17:41:13 +0800

    classifier

---
+++ classifier.py
@@ -0,0 +1,10 @@
+from fastai.vision.all import *
+learn = load_learner('stage1.pkl')
+
+categories1 = 'discarded clothing', 'food waste', 'plastic bags', 'recyc_no_scrap', 'scrap metal piece', 'wood scraps'
+
+def classify_stage1(img):
+    pred, idx, probs = learn.predict(img)
+    return dict(zip(categories1, map(float,probs)))
+
+print(classify_stage1('stage1ex1_t.jpeg'))
