06f55fd46c729d9ad284fc10f2fffe6131001b7d
w-yuchen <yuchenwang1997@gmail.com>
2022-07-09 23:25:44 +0800

    Working photo classifier

---
+++ api_classifier.py
@@ -0,0 +1,28 @@
+from asyncio import sleep
+from typing import Coroutine
+import aiohttp
+
+PUSH_ENDPOINT = "https://hf.space/embed/dpv/Stage1Recycling/api/queue/push/"
+RESULT_ENDPOINT = "https://hf.space/embed/dpv/Stage1Recycling/api/queue/status/"
+
+async def classify_image(source: str) -> Coroutine: 
+    payload = {
+            "action": "predict", 
+            "data": [f"data:image/jpeg;base64,{source}"], 
+            "fn_index": 0,
+            "session_hash": "q7yir8aa60"
+        }
+    
+    async with aiohttp.ClientSession() as session: 
+        async with session.post(PUSH_ENDPOINT, json=payload) as response: 
+            hash = await response.json()
+            hash = hash["hash"]
+            while True:
+                async with session.post(RESULT_ENDPOINT, json={"hash": hash}) as res: 
+                    data = await res.json()
+                    print(data)
+                    if data["status"] == "COMPLETE": 
+                        return data["data"]["data"][0]['label']
+                    if data["status"] != "PENDING": 
+                        return data["status"]
+                await sleep(0.2)
---
+++ bot.py
@@ -0,0 +1,127 @@
+from base64 import b64encode
+import io
+import logging
+from PIL import Image
+import os
+PORT = int(os.environ.get('PORT', 5000))
+
+from dotenv import dotenv_values
+TOKEN = dotenv_values(".env")["BOT_TOKEN"]
+
+from telegram import ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+
+from api_classifier import classify_image
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+GENDER, PHOTO, LOCATION, BIO = range(4)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+
+    await update.message.reply_text(
+        "Please send me a photo of your trash. ",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return PHOTO
+
+
+async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the photo and asks for a location."""
+    user = update.message.from_user
+    photo_file = await update.message.photo[-1].get_file()
+
+    img_buffer = io.BytesIO()
+
+    await photo_file.download(out=img_buffer)
+
+    img = Image.open(img_buffer)
+    img.thumbnail((256, 256))
+    
+    buff = io.BytesIO()
+    img.save(buff, format="JPEG")
+    img_str = b64encode(buff.getvalue())
+
+    encoded = img_str.decode("utf-8")
+
+    result = await classify_image(encoded)
+
+    await update.message.reply_text(
+        f"Your are looking at {str(result)}. "
+    )
+
+    return ConversationHandler.END
+
+
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the location and asks for some info about the user."""
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info(
+        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
+    )
+    await update.message.reply_text(
+        "Maybe I can visit you sometime! At last, tell me something about yourself."
+    )
+
+    return BIO
+
+
+async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Skips the location and asks for info about the user."""
+    user = update.message.from_user
+    logger.info("User %s did not send a location.", user.first_name)
+    await update.message.reply_text(
+        "You seem a bit paranoid! At last, tell me something about yourself."
+    )
+
+    return BIO
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TOKEN).build()
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            PHOTO: [MessageHandler(filters.PHOTO, photo)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    application.add_handler(conv_handler)
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
