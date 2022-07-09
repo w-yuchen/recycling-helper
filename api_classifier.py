129164fb4bacd9bda6811a67caee940983300852
w-yuchen <yuchenwang1997@gmail.com>
2022-07-10 00:47:04 +0800

    Improve classifier

--- api_classifier.py
+++ api_classifier.py
@@ -5,6 +5,9 @@ import aiohttp
 PUSH_ENDPOINT = "https://hf.space/embed/dpv/Stage1Recycling/api/queue/push/"
 RESULT_ENDPOINT = "https://hf.space/embed/dpv/Stage1Recycling/api/queue/status/"
 
+PUSH_ENDPOINT2 = "https://hf.space/embed/dpv/Stage2Recycling/api/predict/"
+RESULT_ENDPOINT2 = "https://huggingface.co/api/spaces/dpv/Stage2Recycling/status/"
+
 async def classify_image(source: str) -> Coroutine: 
     payload = {
             "action": "predict", 
@@ -22,7 +25,33 @@ async def classify_image(source: str) -> Coroutine:
                     data = await res.json()
                     print(data)
                     if data["status"] == "COMPLETE": 
+                        top_labels, round2 = await process_results(data["data"]["data"][0]['confidences'])
+                        if len(round2) > 0: 
+                            top_labels = top_labels + await process_round2(round2[0]['confidence'], payload, session)
                         return True, data["data"]["data"][0]['label']
                     if data["status"] == "FAILED": 
                         return False, data["status"]
                 await sleep(0.2)
+
+async def process_results(results): 
+    highest = results[0]['confidence']
+    labels = []
+    top_labels = filter(lambda x: highest - x['confidence'] < 0.2, results)
+    return top_labels, filter(lambda x: x['label'] == 'recyc_no_scrap', top_labels)
+
+async def process_round2(item, payload, session): 
+    async with session.post(PUSH_ENDPOINT2, json=payload) as response: 
+        hash = await response.json()
+        hash = hash["hash"]
+        while True:
+            async with session.post(RESULT_ENDPOINT2, json={"hash": hash}) as res: 
+                data = await res.json()
+                print(data)
+                if data["status"] == "COMPLETE": 
+                    top_labels, _ = await process_results(data["data"]["data"][0]['confidences'])
+                    total_confidence = sum(top_labels.map(lambda x: x['confidence'], top_labels))
+                    
+                    return True, data["data"]["data"][0]['label']
+                if data["status"] == "FAILED": 
+                    return False, data["status"]
+            await sleep(0.2)
